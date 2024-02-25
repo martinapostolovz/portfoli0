a12f0f8548e7cc3cf1a496c45cfd2b6a3f3c7e9d
martinapostolov1 <prgaming633@gmail.com>
2024-02-26 00:32:21 +0100

    docs

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
-const root = resolve(__dirname, 'src')
+const root = resolve(__dirname, 'docs')
 const outDir = resolve(__dirname, 'dist')
 
 
