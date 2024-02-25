8e270d8c228208049bba88a8ed7cb99e7b2efb28
martinapostolov1 <prgaming633@gmail.com>
2024-02-26 00:23:07 +0100

    router

---
+++ src/about/about.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>jkjjijijfgd</h1>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
+
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/portfoli0/",
+  root,
   plugins: [react()],
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        about: resolve(root, 'about/index.html'),
+      }
+    }
+  }
 })
