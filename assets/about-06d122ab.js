11f9e16619efd70750bbae0b562aa0c26f82ade5
martinapostolov1 <prgaming633@gmail.com>
2024-02-26 02:16:11 +0100

    Updates

--- assets/about-06d122ab.js
+++
@@ -1,1 +0,0 @@
-import{c as t,j as o,R as c,b as e}from"./index-af962549.js";t.render(o(c.StrictMode,{children:[e("h1",{children:"about"}),e("h1",{children:"poipoi"})]}),document.getElementById("root"));
