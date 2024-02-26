7659eb285e464ed3eddae11e6efa1e6da260d7c4
martinapostolov1 <prgaming633@gmail.com>
2024-02-26 23:06:19 +0100

    first big portfolio update

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,34 @@ function App() {
 
   return (
     <div className="App">
-      <img className='darko' src="/images/darko.png" alt="" />
-      <br />
-      <p>darko e peder?</p>
-      <a href='/portfoli0/about/'><button>see more?</button></a>
+      <header className='header-background'>
+        <div className='name-header'>
+          <p className='name'>Martin Apostolov</p>
+        </div>
+        <div className='header-button-no-mobile'>
+          <button id='About-nomobile'>About</button>
+          <button id='Experience-nomobile'>Experience</button>
+          <button id='Contact-nomobile'>Contact</button>
+        </div>
+        <div className='header-button-mobile'>
+          <div className='menu-logo'>
+            <div id='line' className='line'></div>
+            <div id='line2' className='line'></div>
+            <div id='line3' className='line'></div>
+          </div>
+          <hr className='under-menu-logo-line' />
+          <div className='buttons-menu-mobile'>
+            <button className='about-button-mobile'>About</button>
+            <hr id='under-linei' className='under-menu-logo-line' />
+            <button className='experience-button-mobile'>Experience</button>
+            <hr id='under-linei' className='under-menu-logo-line' />
+            <button className='contact-button-mobile'>Contact</button>
+          </div>
+        </div>
+      </header>
+      <div className='background'>
+
+      </div>
     </div>
   )
 }
