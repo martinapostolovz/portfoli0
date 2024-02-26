ed31c3b44503c18af7a1cf563f44ecfa531ef93f
martinapostolov1 <prgaming633@gmail.com>
2024-02-26 02:20:13 +0100

    display-update

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -5,9 +5,9 @@ import './about.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>about</h1>
-    <h1>poipoi</h1>
-    <h1>iooii</h1>
+    <div className="k">
+
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 )
